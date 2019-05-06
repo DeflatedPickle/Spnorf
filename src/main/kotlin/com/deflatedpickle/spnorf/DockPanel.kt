38a0bccff630cad6ffd09ded074b8cc8ffe1a746
Dibble <DeflatedPickle@gmail.com>
2019-05-06 10:39:08 +0100

    Added drag-and-drop

--- src/main/kotlin/com/deflatedpickle/spnorf/DockPanel.kt
+++ src/main/kotlin/com/deflatedpickle/spnorf/DockPanel.kt
@@ -1,7 +1,13 @@
 package com.deflatedpickle.spnorf
 
 import java.awt.*
+import java.awt.datatransfer.DataFlavor
+import java.awt.dnd.DnDConstants
+import java.awt.dnd.DropTarget
+import java.awt.dnd.DropTargetDragEvent
+import java.awt.dnd.DropTargetDropEvent
 import java.awt.event.ActionListener
+import java.io.File
 import javax.swing.JPanel
 import javax.swing.SwingUtilities
 import javax.swing.Timer
@@ -9,35 +15,31 @@ import javax.swing.Timer
 class DockPanel(val orientation: Orientation, val paddingInsets: Insets) : JPanel() {
     val gridBagLayout = GridBagLayout()
 
-    val barButtons = mutableListOf<DockButton>()
-
     // Config Options
-    val barIcons = mutableListOf<String>(
-    )
+    val barIconFiles = mutableListOf<File>()
+    val barIconButtons = mutableListOf<DockButton>()
     val barWidth = 20
     val barHeight = 20
 
     val distanceLimit = Rectangle(60, 10, 60, 10)
 
+    var row = 0
+    var column = 0
+
     init {
         isOpaque = false
 
-        var row = 0
-        var column = 0
-        for (i in barIcons) {
-            // val shellFolder = ShellFolder.getShellFolder(File(i))
-            // val icon = ImageIcon(shellFolder.getIcon(true))
-            barButtons.add(
-                DockButton(i.substringAfterLast("\\").substringBefore(".").capitalize()).also {
-                    it.row = row
-                    it.column = column
-                    this.gridBagLayout.setConstraints(it, GridBagConstraints(column, row, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH, paddingInsets, 1, 1))
-                    this.add(it)
+        this.dropTarget = object : DropTarget() {
+            override fun drop(dtde: DropTargetDropEvent) {
+                // super.drop(dtde)
+
+                dtde.acceptDrop(DnDConstants.ACTION_COPY)
+                val droppedFiles = dtde.transferable.getTransferData(DataFlavor.javaFileListFlavor) as List<File>
+
+                for (file in droppedFiles) {
+                    barIconFiles.add(file)
+                    addButton(file)
                 }
-            )
-            when (this.orientation) {
-                Orientation.HORIZONTAL -> column++
-                Orientation.VERTICAL -> row++
             }
         }
 
@@ -51,7 +53,7 @@ class DockPanel(val orientation: Orientation, val paddingInsets: Insets) : JPane
 
         val timer = Timer(1000 / 60, ActionListener {
             var totalWeights = 0
-            for (i in this.barButtons) {
+            for (i in this.barIconButtons) {
                 val mouseLocation = MouseInfo.getPointerInfo().location
 
                 val frameLocation = GlobalValues.frame!!.location
@@ -60,7 +62,7 @@ class DockPanel(val orientation: Orientation, val paddingInsets: Insets) : JPane
                 // If the mouse is outside the range border
                 val rectangle = Rectangle(frameLocation.x - distanceLimit.x, frameLocation.y - distanceLimit.y, frameSize.width + (distanceLimit.width * 2), frameSize.height + (distanceLimit.height * 2))
                 if (!rectangle.contains(mouseLocation)) {
-                    i.weight = 100 / this.barButtons.size
+                    i.weight = 100 / this.barIconButtons.size
                     continue
                 }
 
@@ -76,7 +78,7 @@ class DockPanel(val orientation: Orientation, val paddingInsets: Insets) : JPane
                 totalWeights += i.weight
             }
 
-            for (i in this.barButtons) {
+            for (i in this.barIconButtons) {
                 val percentage = weightMax - when (highlightType) {
                     HighlightType.MULTIPLIER -> (i.weight * weightMultiplier)
                     HighlightType.POWER -> (Math.pow(i.weight.toDouble(), weightMultiplier))
@@ -102,6 +104,23 @@ class DockPanel(val orientation: Orientation, val paddingInsets: Insets) : JPane
         when (this.orientation) {
             Orientation.HORIZONTAL -> g.fillRect(0, this.height - barHeight, this.width, this.height)
             Orientation.VERTICAL -> g.fillRect(0, 0, barWidth, this.height)
+        }
+    }
+
+    fun addButton(file: File) {
+        // val shellFolder = ShellFolder.getShellFolder(File(i))
+        // val icon = ImageIcon(shellFolder.getIcon(true))
+        barIconButtons.add(
+            DockButton(file.name.substringAfterLast("\\").substringBefore(".").capitalize()).also {
+                it.row = row
+                it.column = column
+                this.gridBagLayout.setConstraints(it, GridBagConstraints(column, row, 1, 1, 1.0, 1.0, GridBagConstraints.WEST, GridBagConstraints.BOTH, paddingInsets, 1, 1))
+                this.add(it)
+            }
+        )
+        when (this.orientation) {
+            Orientation.HORIZONTAL -> column++
+            Orientation.VERTICAL -> row++
         }
     }
 }
